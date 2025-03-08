cf3faea5a77a3bc8aba2aade03d271d1d01f8476
RectorDev <Rector.dev@gmail.com>
2025-03-08 17:36:48 +0330

    commi

--- app/sections/Testimonials/TestimonialCard.tsx
+++ app/sections/Testimonials/TestimonialCard.tsx
@@ -1,10 +1,11 @@
 export default function TestimonialCard({ quote, author, role }: { role: string; quote: string; author: string }) {
   return (
-    <div className="p-4 para bg-accent text-secondary w-[450px]">
-      <div>{quote}</div>
-      <div>
-        <p>{author}</p>
-        <p>{role}</p>
+    <div className=" ">
+      <div className="para text-white max-w-[606px] max-sm:p-7 mx-4 p-[50px] border  border-accent rounded-[45px]"><q>{quote}</q></div>
+      <div className="mt-10 ml-10 max-sm:ml-16">
+        <p className="head-4 text-accent">{author}</p>
+        <p className="para text-white">{role}</p>
+
       </div>
     </div>
   );
--- app/sections/Testimonials/Testimonials.tsx
+++ app/sections/Testimonials/Testimonials.tsx
@@ -4,36 +4,48 @@ import TestimonialCard from "@/app/sections/Testimonials/TestimonialCard";
 import { TESTIMONIALS_SECTION } from "../constants";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation, Pagination, Parallax } from "swiper/modules";
-import "swiper/css/pagination";
 import "swiper/css";
-import "swiper/css/parallax";
+import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { useRef } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 export default function Testimonials() {
   return (
     <section className="container-padding  margin-y-2xl">
       <div>
         <SectionHead description={TESTIMONIALS_SECTION.description} head={TESTIMONIALS_SECTION.title} />
       </div>
-      <div className="margin-y-2xl">
+      <div className="margin-y-2xl bg-secondary rounded-[45px]">
         <Swiper
+          className="swiper_container"
           centeredSlides={true}
+          centeredSlidesBounds
           initialSlide={3}
+          pagination={{
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet(index, className) {
+              return `<span class=${className}> <Image  fill="#fff" src="/Vector.svg"> </span>`;
+            },
+          }}
+          navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }}
+          modules={[Pagination, Navigation]}
           grabCursor
-          freeMode={{ enabled: true }}
-          slidesPerView={2}
-          spaceBetween={3}
-          parallax={{ enabled: true }}
-          navigation={{ enabled: true }}
-          pagination={{ enabled: true }}
-          modules={[Parallax, Pagination, Navigation,FreeMode]}
+          slidesPerView={"auto"}
           slideToClickedSlide
         >
           {TESTIMONIALS_SECTION.testimonials.map((tes, index) => (
-            <SwiperSlide  key={index}>
+            <SwiperSlide className="margin-y-2xl" key={index}>
               <TestimonialCard author={tes.author} quote={tes.quote} role={tes.role} />
             </SwiperSlide>
           ))}
+
+          <div className="swiper-button-prev">
+            <span>
+              <ArrowLeft className="px-4 py-2" size={24} />
+            </span>
+          </div>
+          <div className="swiper-button-next">--?</div>
+          <div className="swiper-pagination"></div>
         </Swiper>
       </div>
     </section>
